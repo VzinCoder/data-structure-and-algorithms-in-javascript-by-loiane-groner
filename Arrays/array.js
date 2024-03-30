cd13cd2623a6bb133d586a271f567ab12ce551f8
VzinCoder <contato.viniciusp.b@gmail.com>
2024-03-30 17:21:40 -0300

    Arrays

---
+++ Arrays/array.js
@@ -0,0 +1,148 @@
+// criando array
+let arrayNumbers = [1, 2, 3, 4, 5]
+
+
+// acessar elemento do array
+logSeparator('acessando elemento')
+
+const firstElement = arrayNumbers[0] //retorna o primeiro elemento e atribui a variavel
+console.log(firstElement) // exibe 1
+
+// acessar comprimento do array
+logSeparator('acessando comprimento')
+
+const sizeArr = arrayNumbers.length //retorna o comprimento do arr e atribui a variavel
+console.log(sizeArr) // exibe 5
+
+// percorrer array
+logSeparator('exibindo todos os elementos do arr')
+
+for (let i = 0; i < arrayNumbers.length; i++) {
+    console.log(arrayNumbers[i]) // exibe os 5 numeros do array
+}
+
+
+//exibindo os 20 primeiros valores da sequencia de fib
+logSeparator('fib')
+
+const fib = new Array(20).fill(null)  // cria arr de tamanho 20
+
+fib[0] = 0
+fib[1] = 1
+
+for (let i = 2; i <= 20; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+}
+
+console.log(fib.join(',')) // join retorna uma string baseada no arr onde cada elemento é separado por ','
+
+
+//adicinar novo elemento no arr
+logSeparator('adicionando elemento')
+
+console.log(arrayNumbers) // exibe o arr
+
+arrayNumbers.push(10) // adiciona 10 ao final do arr
+
+console.log(arrayNumbers)// exibe o arr
+
+//adicinar elemento na primeira posicao do arr
+logSeparator('adicionando elemento na primeira posicao')
+
+console.log(arrayNumbers) // exibe o arr
+
+arrayNumbers.unshift(10) // adiciona 10 ao final do arr
+
+console.log(arrayNumbers)// exibe o arr
+
+//removendo ultimo elemento do arr
+logSeparator('removendo elemento da ultima posicao')
+
+console.log(arrayNumbers) // exibe o arr
+
+arrayNumbers.pop() // adiciona 10 ao final do arr
+
+console.log(arrayNumbers)// exibe o arr
+
+//removendo primeiro elemento do arr
+logSeparator('removendo elemento da primeira posicao')
+
+console.log(arrayNumbers) // exibe o arr
+
+arrayNumbers.shift() // adiciona 10 ao final do arr
+
+console.log(arrayNumbers)// exibe o arr
+
+
+// adicionando e removendo elementos sem usar metodos da linguagem
+logSeparator('adicionando e removendo elementos sem usar metodos da linguagem')
+
+Array.prototype.insertFirstPosition = function (value) {
+
+    for (let i = this.length; i > 0; i--) {
+        this[i] = this[i - 1]
+    }
+
+    this[0] = value
+}
+
+Array.prototype.reIndex = function () {
+    const newArr = []
+
+    let indexNewArr = 0
+    for (let i = 0; i < this.length; i++) {
+        if (typeof this[i] !== 'undefined') {
+            newArr[indexNewArr++] = this[i]
+        }
+    }
+
+    return newArr
+}
+
+Array.prototype.removeFirstPosition = function () {
+
+    for (let i = 0; i < this.length; i++) {
+        this[i] = this[i + 1]
+    }
+
+    return this.reIndex()
+}
+
+console.log(arrayNumbers)
+arrayNumbers.insertFirstPosition(10)
+console.log(arrayNumbers)
+
+arrayNumbers = arrayNumbers.removeFirstPosition()
+console.log(arrayNumbers)
+
+// adicionando e removendo elementos de uma posicao especifica 
+logSeparator('adicionando e removendo elementos de uma posicao especifica')
+
+
+console.log(arrayNumbers)
+arrayNumbers.splice(3, 0, 4, 5) // adiciona 4 e 5 a partir do indice 3(inclusive)
+console.log(arrayNumbers)
+
+arrayNumbers.splice(3, 2)  // remove 2 elemento da posicao apartir do indice 3(inclusive)
+console.log(arrayNumbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function logSeparator(separator = "") {
+    console.log(`=====${separator.replaceAll(' ', '-')}=====`)
+}
+
+
+
---
+++ Arrays/infoArray.md
@@ -0,0 +1,25 @@
+# Por que usar arrays e quando usar
+
+Um array é uma estrutura de dados que armazena uma coleção de elementos do mesmo tipo de dados de forma sequencial. Eles são amplamente utilizados na programação devido à sua capacidade de armazenar múltiplos valores em uma única variável.
+
+## Por que usar arrays:
+
+1. **Armazenamento eficiente de dados**: Arrays permitem armazenar múltiplos valores em uma única variável, economizando espaço na memória e facilitando a manipulação de conjuntos de dados.
+
+2. **Acesso rápido aos elementos**: Os elementos em um array são acessados por meio de um índice, o que permite um acesso rápido e direto a qualquer elemento com base na sua posição.
+
+3. **Facilidade de manipulação de conjuntos de dados**: Arrays facilitam operações como adição, remoção e modificação de elementos, o que é útil ao lidar com conjuntos de dados.
+
+4. **Eficiência algorítmica**: Muitos algoritmos e estruturas de dados dependem do uso de arrays devido à sua eficiência em termos de tempo de acesso e manipulação.
+
+## Quando usar arrays:
+
+1. **Armazenamento de coleções de dados**: Use arrays quando precisar armazenar e manipular coleções de dados do mesmo tipo, como uma lista de números, nomes de pessoas, ou qualquer outro conjunto de elementos.
+
+2. **Iteração sobre elementos**: Quando precisar percorrer todos os elementos de um conjunto de dados e realizar operações em cada um deles, os arrays são uma escolha eficiente.
+
+3. **Implementação de algoritmos**: Algoritmos que dependem de acessar elementos de forma sequencial ou aleatória se beneficiam do uso de arrays, como algoritmos de ordenação, pesquisa e processamento de dados.
+
+4. **Acesso rápido a elementos específicos**: Se precisar acessar rapidamente elementos específicos com base em sua posição, os arrays são uma escolha adequada devido ao acesso direto por meio de índices.
+
+Em resumo, use arrays sempre que precisar armazenar e manipular uma coleção de dados do mesmo tipo de forma eficiente e acessível.
