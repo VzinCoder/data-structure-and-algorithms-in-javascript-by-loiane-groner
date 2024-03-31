5b04d981b43a097c7721b2ae663a21a1c5b4ba01
VzinCoder <contato.viniciusp.b@gmail.com>
2024-03-31 13:06:17 -0300

    Added functions to add and remove elements from a specific position without language methods

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,4 +1,4 @@
-const {logSeparator} = require('../logSeparator')
+const { logSeparator } = require('../logSeparator')
 
 // criando array
 let arrayNumbers = [1, 2, 3, 4, 5]
@@ -125,5 +125,42 @@ console.log(arrayNumbers)
 arrayNumbers.splice(3, 0, 4, 5) // adiciona 4 e 5 a partir do indice 3(inclusive)
 console.log(arrayNumbers)
 
-arrayNumbers.splice(3, 2)  // remove 2 elemento da posicao apartir do indice 3(inclusive)
+arrayNumbers.splice(3, 2)  // remove 2 elementos apartir do indice 3(inclusive)
+console.log(arrayNumbers)
+
+// adicionando e removendo elementos de uma posicao especifica sem metodos da linguagem
+logSeparator('adicionando e removendo elementos de uma posicao especifica sem metodos da linguagem')
+
+
+Array.prototype.addElementInIndex = function (element, index) {
+
+    if(index > this.length || index < 0){
+        throw new Error('Position invalid')
+    }
+
+    for (let i = this.length; i > index; i--) {
+        this[i] = this[i - 1]
+    }
+
+    this[index] = element
+}
+
+Array.prototype.removeElementInIndex = function (index) {
+
+    if(index >= this.length || index < 0){
+        throw new Error('Position invalid')
+    }
+
+    for (let i = index; i < this.length; i++) {
+        this[i] = this[i+1]
+    }
+
+    return this.reIndex()
+}
+
+console.log(arrayNumbers)
+arrayNumbers.addElementInIndex(10,2)
+console.log(arrayNumbers)
+
+arrayNumbers = arrayNumbers.removeElementInIndex(2)
 console.log(arrayNumbers)
